9f7d8cddef3ff3d0a2dff6ca025aadd9725209f7
leonard475192 <leonard.t1028@gmail.com>
2022-03-27 14:13:31 +0900

    :+1: feat: 本返却時 rental history insert

--- src/main/kotlin/com/book/manager/domain/model/RentalHistory.kt
+++ src/main/kotlin/com/book/manager/domain/model/RentalHistory.kt
@@ -3,7 +3,7 @@ package com.book.manager.domain.model
 import java.time.LocalDateTime
 
 data class RentalHistory (
-    val id: Long,
+    val id: Long? = null,
     val rentalDatetime: LocalDateTime,
     val returnDatetime: LocalDateTime
 )
